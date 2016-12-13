5eb11681e656098d77297a6c120bd799a91d2c3b
Bojan <bojan.martinovic@qacube.com>
2016-12-13 11:42:29 +0100

    List names from addressbook

---
+++ src/main/java/com/bojan/addressbook/controller/AddressbookController.java
@@ -0,0 +1,20 @@
+package com.bojan.addressbook.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bojan.addressbook.repository.PersonRepository;
+
+@Controller
+public class AddressbookController {
+	@Autowired
+	PersonRepository personRepository;
+
+	@GetMapping("/")
+	public String handle(Model model) {
+		model.addAttribute("persons", personRepository.findAll());
+		return "addressbook";
+	}
+}
